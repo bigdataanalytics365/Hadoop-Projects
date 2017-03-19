cb8859659935622d284b587c8fe3afe4041ab51d
Rushabh Shah <rushabh13shah@gmail.com>
2017-03-19 15:26:49 -0500

    Moving average analysis complete

--- PigScripts/MovingAverage.java
+++ PigScripts/MovingAverage.java
@@ -12,8 +12,8 @@ import org.apache.pig.data.DataBag;
 
 public class MovingAverage extends EvalFunc<DataBag>{
     
-    Public final BagFactory bagFactory = BagFactory.getInstance();
-    Public final TupleFactory tupleFactory = TupleFactory.getInstance();
+    public final BagFactory bagFactory = BagFactory.getInstance();
+    public final TupleFactory tupleFactory = TupleFactory.getInstance();
 
     public DataBag exec(Tuple input) throws IOException {
         if (input == null || input.size() == 0){
@@ -24,55 +24,70 @@ public class MovingAverage extends EvalFunc<DataBag>{
         // Output DataBag. This bag contains the moving average for the entire month of october for this particular company.
         DataBag output = bagFactory.newDefaultBag();
         // Prices to take the average of.
-        LinkedList<float> prices = new LinkedList<float>();
+        LinkedList<Float> prices = new LinkedList<Float>();
         // Counters for days in october and number of prices respectively.
-        int day = 1;
         int i = 1;
+        int day = 1;
         // Last moving average;
-        float lastMovingAverage;
+        float lastMovingAverage = 0;
         
         for (Tuple temp : inputBag) {
+            String companyName = temp.get(0).toString();
             // Open price of stock for this day.
             float stockPrice = new Float(temp.get(2).toString()).floatValue();            
             // Check if we need to start computing moving average
-            if(i > 20){
-                // Average Record for this day for this company.
-                Tuple averageRecord = tupleFactory.newTuple();
-                // Append the company name to this tuple.
-                averageRecord.append(temp.get(0).toString());
-                // Append the date to this tuple.
-                averageRecord.append(temp.get(1).toString());
-
+            if(i >= 20){
                 int stockDay = Integer.parseInt(temp.get(1).toString().substring(6,8));
                 // Number of days this stock was not shown.
                 int dayDiff = stockDay - day;
                 for(int d=day; d<(dayDiff+day); d++){
                     // Append the averageRecord to output for missing days from last available prices.
-                    Tuple t = tupleFactory.newTuple();
-                    t.append(temp.get(0).toString());
+                    Tuple missingRecord = tupleFactory.newTuple();
+                    // Append company name.
+                    missingRecord.append(companyName);
+                    // Append the date for this missingRecord.
                     if(day < 10){
-                        t.append("2013100"+day.toString());
+                        missingRecord.append("2013100"+Integer.toString(d));
                     }else{
-                        t.append("201310"+day.toString());
+                        missingRecord.append("201310"+Integer.toString(d));
                     }
-                    t.append(lastMovingAverage);
+                    // Append open price and moving average.
+                    missingRecord.append(stockPrice);
+                    missingRecord.append(lastMovingAverage);
+                    // Add this record to the output.
+                    output.add(missingRecord);
                 }
                 // Start computing averages.
                 day = stockDay + 1;
+                // Compute the average for this day.
+                float avg = computeAverage(prices);
+                // Average Record for this day for this company.
+                Tuple averageRecord = tupleFactory.newTuple();
+                // Append the company name, date and average to this tuple.
+                averageRecord.append(companyName);
+                averageRecord.append(temp.get(1).toString());
+                averageRecord.append(stockPrice);
+                averageRecord.append(avg);
+                // Add this tuple to the output bag.
+                output.add(averageRecord);
+                // Update the prices queue.
+                prices.removeFirst();
+                prices.add(new Float(stockPrice));
+                lastMovingAverage = avg;
             }else{
-                // Buildup the price values to use.
-                prices.add(openPrice);
+                // Buildup the price values to use for first average.
+                prices.add(new Float(stockPrice));
             }
             i++;
         }
-        // Output the DataBag we have constructed here. This bag contains the moving average for the entire month of october for this particular company.
+        // Output the DataBag we have constructed here. This bag contains moving averages for the entire month of october for this particular company.
         return output;
     }
     
-    public float computeAverage(LinkedList<float> prices){
-        float total;
+    public float computeAverage(LinkedList<Float> prices){
+        float total = 0;
         for (int i = 0; i < prices.size(); i++) {
-			total += prices.get(i);
+			total += prices.get(i).floatValue();
 		}
         return total / prices.size();
     }
--- PigScripts/MovingAverage.pig
+++ PigScripts/MovingAverage.pig
@@ -1,30 +1,37 @@
 -- Register the jar file
 REGISTER '/home/rvshah/lab7/stockanalysis.jar';
 -- LOAD data from csv file
--- file_input = LOAD '/class/s17419/lab7/historicaldata.csv' USING PigStorage(',') AS (ticker:chararray, date:long, open:float, high:float, low:float, close:float, volume:long);
-file_input = LOAD '/home/rvshah/lab7/test.csv' USING PigStorage(',') AS (ticker:chararray, date:long, open:chararray, high:chararray, low:chararray, close:chararray, volume:long);
+file_input = LOAD '/class/s17419/lab7/historicaldata.csv' USING PigStorage(',') AS (ticker:chararray, date:long, open:float, high:float, low:float, close:float, volume:long);
+-- file_input = LOAD '/home/rvshah/lab7/test.csv' USING PigStorage(',') AS (ticker:chararray, date:long, open:chararray, high:chararray, low:chararray, close:chararray, volume:long);
 input_data = FOREACH file_input GENERATE ticker AS company, date, open;
+-- All records before Oct 1st 2013.
 first_date_data = FILTER input_data BY date <= 20131001;
+-- All records before Oct 31st 2013.
 filtered_data = FILTER input_data BY date <= 20131031;
 -- Group the input data by company.
 grouped_data = GROUP filtered_data BY company;
+-- Compute the starting dates for each company.
 start_data = FOREACH (GROUP first_date_data BY company) {
     dates = first_date_data.date;
     ordered_dates = ORDER dates BY date DESC;
+    -- Compute the starting date for each company.
     start = stockanalysis.StartDate(ordered_dates);
     GENERATE group AS symbl, start AS first_date;
 }
+-- Join the starting date data with the stock price data.
 joined_data = JOIN filtered_data BY company, start_data BY symbl;
+-- Group everything by the company name/symbl.
 stock_data = GROUP joined_data BY company;
+-- Compute the running averages of companies from grouped data.
 company_averages = FOREACH stock_data {
+    -- Filter out all records except for the desired range.
     filtered_dates = FILTER joined_data BY date >= first_date;
-    avgs = FOREACH filtered_dates GENERATE stockanalysis.MovingAverage(filtered_dates);
-    GENERATE company, avgs;
+    -- Order the records in ASC fashion so we compute the correct averages for each date in october.
+    ordered_dates = ORDER filtered_dates BY date ASC;
+    GENERATE FLATTEN(stockanalysis.MovingAverage(ordered_dates)) AS avgs;
 }
--- jf_data = FILTER joined_data BY filtered_data.date >= start_data.first_date;
-test = LIMIT company_averages 5;
+-- Dump the result averages for first company.
+test = LIMIT company_averages 31;
 DUMP test;
-
--- -- Store the growths in respective places.
--- STORE largest_growth_one INTO '/scr/rvshah/lab7/exp1/first_period/';
--- STORE largest_growth_two INTO '/scr/rvshah/lab7/exp1/second_period/';
+-- Store the averages in hdfs.
+STORE company_averages INTO '/scr/rvshah/lab7/exp2/moving_averages/';
