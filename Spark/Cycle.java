2fad84d0b8f7a9c1f443b146043b3aa1984a1874
Rushabh Shah <rushabh13shah@gmail.com>
2017-04-05 20:12:59 -0500

    Completed implementation of Directed Cycles

--- Spark/Cycle.java
+++ Spark/Cycle.java
@@ -6,6 +6,7 @@ import org.apache.spark.api.java.function.*;
 import org.apache.spark.api.java.function.PairFunction;
 
 import scala.Tuple2;
+import java.util.*;
 
 public class Cycle {
 
@@ -24,23 +25,60 @@ public class Cycle {
 		JavaRDD<String> patent_input = context.textFile(args[0]);
 		
 		/**
-		 * This maps the vertices in for each edge as they are read from the file in following format.
-		 * Key = From vertex, Values = Edge(fromVertex, toVertex) as a String
+		 * Map the key value pair in order as they are read.
+		 * Key = fromVertex, Values = toVertex
 		 */
-		JavaPairRDD<String,String> patents = patent_input.mapToPair(new PairFunction<String, String, String>() {
+		JavaPairRDD<String,String> citations = patent_input.mapToPair(new PairFunction<String, String, String>() {
 			@Override
 			public Tuple2<String, String> call(String s) throws Exception {
 				String[] tokens = s.split("\\s+");
 				String fromVertex = tokens[0];
 				String toVertex = tokens[1];
-				
-				String key = fromVertex;
-				String value = fromVertex+","+toVertex;
-				return new Tuple2<String, String>(key, value);
+				return new Tuple2<String, String>(fromVertex, toVertex+"TO");
 			}
 		});
-			
-		results.saveAsTextFile(args[1]);
+		
+		/**
+		 * Map the key value pair in order as they are read.
+		 * Key = toVertex, Values = fromVertex
+		 */
+		JavaPairRDD<String,String> citations_reversed = patent_input.mapToPair(new PairFunction<String, String, String>() {
+			@Override
+			public Tuple2<String, String> call(String s) throws Exception {
+				String[] tokens = s.split("\\s+");
+				String fromVertex = tokens[0];
+				String toVertex = tokens[1];
+				return new Tuple2<String, String>(toVertex, fromVertex+"FROM");
+			}
+		});
+		
+		/**
+		* This join gives the key it joined by with vertexFrom and vertexTo in the same tuple that was mapped above, forming the 'open triads'
+		*/
+		JavaPairRDD<String,Tuple2<String,String>> joined = citations.join(citations_reversed);
+		
+		/**
+		 * This will extract out all the edges that will need to be present in the original graph which will make a one complete cycle of length 3.
+		 */
+		JavaPairRDD<String,String> edges_to_check = joined.mapToPair(new PairFunction<Tuple2<String,Tuple2<String,String>>, String, String>() {
+			@Override
+			public Tuple2<String, String> call(Tuple2<String,Tuple2<String,String>> s) throws Exception {
+				Tuple2<String,String> adj = s._2;
+				String to = adj._1;
+				String from = adj._2;
+				// Added TO at the end so it matches with the first 'citation' map pair function during intersection.
+				return new Tuple2<String,String>(to.substring(0,to.length()-2),from.substring(0,from.length()-4)+"TO");
+			}
+		});
+		
+		/**
+		 * Intersect with the original citation RDD to see what edges are present to enumerate number of cycles with length 3.
+		 * Number of cycles = # of records in matched_edges / 3.
+		 */
+		JavaPairRDD<String,String> matched_edges = citations.intersection(edges_to_check);
+		
+		matched_edges.saveAsTextFile(args[1]);
+		// System.out.println(results.getClass().getName());
 		context.stop();
 		context.close();
 	}
